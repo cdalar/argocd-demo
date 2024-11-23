8e96683f9242c0b02a61435a3d081a0345e20f2e
Cemal Y. Dalar <cemal@dalar.net>
2024-11-23 06:27:07 +0100

    apps ns

--- clusters/cluster-1/generate.sh
+++ clusters/cluster-1/generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 helm template -n longhorn-system longhorn longhorn/longhorn -f longhorn/values-custom.yaml > apps/longhorn/gen.yaml
-helm template -n mariadb mariadb mariadb/mariadb  > apps/mariadb/gen.yaml
+helm template -n apps mariadb mariadb/mariadb  > apps/mariadb/gen.yaml
